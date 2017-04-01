391edc4b158541800955e33295ea6189de8d51ac
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2017-04-01 12:09:28 -0700

    add fname

--- generation/src/RunAction.cc
+++ generation/src/RunAction.cc
@@ -104,7 +104,7 @@ void RunAction::BeginOfRunAction(const G4Run* run)
 
   // Open an output file
   //
-  G4String fileName = "";
+  G4String fileName = "PLEASEWORK";
   analysisManager->OpenFile(fileName);
 }
 
