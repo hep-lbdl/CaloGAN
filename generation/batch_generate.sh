b3a3fa24ef24d3e69f7c8f08aa140bf20f28ab9f
Luke de Oliveira <lukepercivaldeoliveira@gmail.com>
2017-04-01 14:43:10 -0700

    adding batch scripy

---
+++ generation/batch_generate.sh
@@ -0,0 +1,14 @@
+#!/bin/csh
+#$ -j y
+
+
+echo "Assuming working directory: $PBS_O_WORKDIR"
+cd "$PBS_O_WORKDIR"
+
+. cfg/pdsf-env.sh
+
+
+
+GAN_FNAME="generated_file_$SGE_TASK_ID.root"
+
+generate -m cfg/run2.mac
