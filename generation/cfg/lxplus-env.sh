1adb7ab745f038af8b0e56d1396f839dc6435584
bnachman <bnachman@cern.ch>
2021-05-01 04:51:59 +0200

    fixed the lxplus config; had overwritten it with lbnl one

--- generation/cfg/lxplus-env.sh
+++ generation/cfg/lxplus-env.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-
-module load ROOT/6.08.00
-module load Geant4/10.02.b01
+source /cvmfs/sft.cern.ch/lcg/contrib/gcc/6.3.0/x86_64-slc6/setup.sh
+source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb-MT/CMake-setup.sh
+source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb-MT/share/Geant4-10.6.0/geant4make/geant4make.sh
+source /cvmfs/geant4.cern.ch/geant4/10.6/x86_64-slc6-gcc63-optdeb-MT/bin/geant4.sh 
